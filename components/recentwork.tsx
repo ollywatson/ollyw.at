63ea61ebd785a79b116a7dee1a347f130f516463
ollywatson <32039564+ollywatson@users.noreply.github.com>
2025-02-12 11:10:02 +0000

    Tidied up file structure

--- components/recentwork.tsx
+++ components/recentwork.tsx
@@ -3,8 +3,8 @@ import ProjectBox from './projectbox';
 
 const RecentWorkSection = () => {
     return (
-        <div className="mt-[78px]">
-            <p className="text-base text-primary font-bold">Recent Work</p>
+        <div className="mt-[78px] px-[6px]">
+            <p className="text-base md:text-base-md text-primary font-bold px-[20px]">Work</p>
             <ProjectBox />
         </div>
     );
