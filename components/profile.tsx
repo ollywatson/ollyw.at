6bd3339b1a0d661d0abfbf54b768447e908b05d0
ollywatson <32039564+ollywatson@users.noreply.github.com>
2025-02-17 14:59:30 +0000

    Removed font-swap

--- components/profile.tsx
+++ components/profile.tsx
@@ -14,7 +14,7 @@ const Profile = () => {
                 <p className="pr-[52px] text-headline font-serif text-secondary"><span className="text-primary">Olly Watson</span> is a product designer from the UK, currently working at <Link href="https://www.deliveroo.design/" className="inline-link text-primary">Deliveroo</ Link></p>
             </div>
             <div className="mt-[26px]">
-                <p className="text-base text-secondary">Over the past decade, I’ve worked at many stages of the product development life cycle, from ethnographic research to design engineering. I designed and built <Link href="https://www.deliveroo.design/" className="inline-link text-primary font-semibold">Voicey</Link>, an iPhone app for voice messaging.</p>
+                <p className="text-base text-secondary">Over the past decade, I’ve worked at many stages of the product development life cycle, from ethnographic research to design engineering. I designed and built <Link href="https://www.deliveroo.design/" className="inline-link text-primary">Voicey</Link>, an iPhone app for voice messaging.</p>
             </div>
             <div className="mt-[26px] pb-[8px]">
                 <EmailButton />
