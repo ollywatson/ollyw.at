40da11a4928f1116f5a07024bf8c6e63dfd8eacc
ollywatson <32039564+ollywatson@users.noreply.github.com>
2025-02-23 19:12:52 +0000

    Added image preloading in _document.tsx

--- app/_document.tsx
+++ app/_document.tsx
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* <link rel="preload" as="image" href="/multi-basket-1.png" />
+        <link rel="preload" as="image" href="/multi-basket-1.png" />
         <link rel="preload" as="image" href="/multi-basket-2.png" />
         <link rel="preload" as="image" href="/multi-basket-1.png" />
         <link rel="preload" as="image" href="/multi-basket-1.png" />
         <link rel="preload" as="image" href="/multi-basket-1.png" />
         <link rel="preload" as="image" href="/multi-basket-1.png" />
-        <link rel="preload" as="image" href="/multi-basket-1.png" /> */}
+        <link rel="preload" as="image" href="/multi-basket-1.png" />
       </Head>
       <body>
         <Main />
